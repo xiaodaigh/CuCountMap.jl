c4732696462368eeaeb29cecf52987573c5d6f19
xiaodaigh <zhuojia.dai@gmail.com>
2020-08-30 01:46:38 +1000

    reduced test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,14 @@ using SortingAlgorithms
 using Test
 
 @testset "CuCountMap.jl - countmap" begin
-    v = rand(UInt16, 1_000_000_000);
+    v = rand(UInt16, 1_000_000);
     @time a = cucountmap(v);
     @time aa = countmap(v);
     @test a == aa
 end
 
 @testset "CuCountMap.jl - sort" begin
-    v = rand(UInt64, 100_000_000);
+    v = rand(UInt64, 1_000_000);
     @time sorted_v = gpuradixsort(v);
     @time sorted_v_cpu = sort(v, alg=RadixSort);
     @test sorted_v == sorted_v_cpu
